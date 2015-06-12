ffd7b23ad96c37cc79cc7b046ace9aadd6467fbd
Trent Scheffert <trent.scheffert@gmail.com>
2015-06-12 17:53:10 -0500

    Fix: Correct user directory and added .git-template

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,24 +1,25 @@
-mklink /j C:\Users\tscheffert\bin C:\Users\tscheffert\.dotfiles\bin
-mklink /j C:\Users\tscheffert\init C:\Users\tscheffert\.dotfiles\init
-mklink /j C:\Users\tscheffert\source C:\Users\tscheffert\.dotfiles\source
-mklink /j C:\Users\tscheffert\vimfiles C:\Users\tscheffert\.dotfiles\vimfiles
-mklink C:\Users\tscheffert\.bash_profile C:\Users\tscheffert\.dotfiles\.bash_profile
-mklink C:\Users\tscheffert\.bash_prompt C:\Users\tscheffert\.dotfiles\.bash_prompt
-mklink C:\Users\tscheffert\.bashrc C:\Users\tscheffert\.dotfiles\.bashrc
-mklink C:\Users\tscheffert\.curlrc C:\Users\tscheffert\.dotfiles\.curlrc
-mklink C:\Users\tscheffert\.editorconfig C:\Users\tscheffert\.dotfiles\.editorconfig
-mklink C:\Users\tscheffert\.gitconfig C:\Users\tscheffert\.dotfiles\.gitconfig
-mklink C:\Users\tscheffert\.gitignore C:\Users\tscheffert\.dotfiles\.gitignore
-mklink C:\Users\tscheffert\.git-prompt.sh C:\Users\tscheffert\.dotfiles\.git-prompt.sh
-mklink C:\Users\tscheffert\.jscs.json C:\Users\tscheffert\.dotfiles\.jscs.json
-mklink C:\Users\tscheffert\.jshintrc C:\Users\tscheffert\.dotfiles\.jshintrc
-mklink C:\Users\tscheffert\.osx C:\Users\tscheffert\.dotfiles\.osx
-mklink C:\Users\tscheffert\.vimrc C:\Users\tscheffert\.dotfiles\.vimrc
-mklink C:\Users\tscheffert\coffeelint.json C:\Users\tscheffert\.dotfiles\coffeelint.json
-mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\tscheffert\.dotfiles\PowerShell\profile.ps1
-mklink C:\Users\tscheffert\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\tscheffert\.dotfiles\PowerShell\PowerTabConfig.xml
-mklink C:\Users\tscheffert\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\tscheffert\.dotfiles\PowerShell\TabExpansion.xml
-mklink C:\Users\tscheffert\.ptignore C:\Users\tscheffert\.dotfiles\.ptignore
+mklink /j C:\Users\Trent\bin C:\Users\Trent\.dotfiles\bin
+mklink /j C:\Users\Trent\init C:\Users\Trent\.dotfiles\init
+mklink /j C:\Users\Trent\source C:\Users\Trent\.dotfiles\source
+mklink /j C:\Users\Trent\vimfiles C:\Users\Trent\.dotfiles\vimfiles
+mklink /j C:\Users\Trent\.git_template C:\Users\Trent\.dotfiles\.git_template
+mklink C:\Users\Trent\.bash_profile C:\Users\Trent\.dotfiles\.bash_profile
+mklink C:\Users\Trent\.bash_prompt C:\Users\Trent\.dotfiles\.bash_prompt
+mklink C:\Users\Trent\.bashrc C:\Users\Trent\.dotfiles\.bashrc
+mklink C:\Users\Trent\.curlrc C:\Users\Trent\.dotfiles\.curlrc
+mklink C:\Users\Trent\.editorconfig C:\Users\Trent\.dotfiles\.editorconfig
+mklink C:\Users\Trent\.gitconfig C:\Users\Trent\.dotfiles\.gitconfig
+mklink C:\Users\Trent\.gitignore C:\Users\Trent\.dotfiles\.gitignore
+mklink C:\Users\Trent\.git-prompt.sh C:\Users\Trent\.dotfiles\.git-prompt.sh
+mklink C:\Users\Trent\.jscs.json C:\Users\Trent\.dotfiles\.jscs.json
+mklink C:\Users\Trent\.jshintrc C:\Users\Trent\.dotfiles\.jshintrc
+mklink C:\Users\Trent\.osx C:\Users\Trent\.dotfiles\.osx
+mklink C:\Users\Trent\.vimrc C:\Users\Trent\.dotfiles\.vimrc
+mklink C:\Users\Trent\coffeelint.json C:\Users\Trent\.dotfiles\coffeelint.json
+mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\Trent\.dotfiles\PowerShell\profile.ps1
+mklink C:\Users\Trent\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\Trent\.dotfiles\PowerShell\PowerTabConfig.xml
+mklink C:\Users\Trent\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\Trent\.dotfiles\PowerShell\TabExpansion.xml
+mklink C:\Users\Trent\.ptignore C:\Users\Trent\.dotfiles\.ptignore
 
 
 pause

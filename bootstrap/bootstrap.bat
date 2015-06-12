d96d4a15f22cc82af7f1a8c3bcd229a06cff6a04
Trent Scheffert <trent.scheffert@gmail.com>
2015-06-12 17:46:02 -0500

    Fix: Add .git-prompt.sh to bootstrap.bat

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -2,22 +2,23 @@ mklink /j C:\Users\tscheffert\bin C:\Users\tscheffert\.dotfiles\bin
 mklink /j C:\Users\tscheffert\init C:\Users\tscheffert\.dotfiles\init
 mklink /j C:\Users\tscheffert\source C:\Users\tscheffert\.dotfiles\source
 mklink /j C:\Users\tscheffert\vimfiles C:\Users\tscheffert\.dotfiles\vimfiles
-mklink /h C:\Users\tscheffert\.bash_profile C:\Users\tscheffert\.dotfiles\.bash_profile
-mklink /h C:\Users\tscheffert\.bash_prompt C:\Users\tscheffert\.dotfiles\.bash_prompt
-mklink /h C:\Users\tscheffert\.bashrc C:\Users\tscheffert\.dotfiles\.bashrc
-mklink /h C:\Users\tscheffert\.curlrc C:\Users\tscheffert\.dotfiles\.curlrc
-mklink /h C:\Users\tscheffert\.editorconfig C:\Users\tscheffert\.dotfiles\.editorconfig
-mklink /h C:\Users\tscheffert\.gitconfig C:\Users\tscheffert\.dotfiles\.gitconfig
-mklink /h C:\Users\tscheffert\.gitignore C:\Users\tscheffert\.dotfiles\.gitignore
-mklink /h C:\Users\tscheffert\.jscs.json C:\Users\tscheffert\.dotfiles\.jscs.json
-mklink /h C:\Users\tscheffert\.jshintrc C:\Users\tscheffert\.dotfiles\.jshintrc
-mklink /h C:\Users\tscheffert\.osx C:\Users\tscheffert\.dotfiles\.osx
-mklink /h C:\Users\tscheffert\.vimrc C:\Users\tscheffert\.dotfiles\.vimrc
-mklink /h C:\Users\tscheffert\coffeelint.json C:\Users\tscheffert\.dotfiles\coffeelint.json
-mklink /h C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\tscheffert\.dotfiles\PowerShell\profile.ps1
-mklink /h C:\Users\tscheffert\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\tscheffert\.dotfiles\PowerShell\PowerTabConfig.xml
-mklink /h C:\Users\tscheffert\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\tscheffert\.dotfiles\PowerShell\TabExpansion.xml
-mklink /h C:\Users\tscheffert\.ptignore C:\Users\tscheffert\.dotfiles\.ptignore
+mklink C:\Users\tscheffert\.bash_profile C:\Users\tscheffert\.dotfiles\.bash_profile
+mklink C:\Users\tscheffert\.bash_prompt C:\Users\tscheffert\.dotfiles\.bash_prompt
+mklink C:\Users\tscheffert\.bashrc C:\Users\tscheffert\.dotfiles\.bashrc
+mklink C:\Users\tscheffert\.curlrc C:\Users\tscheffert\.dotfiles\.curlrc
+mklink C:\Users\tscheffert\.editorconfig C:\Users\tscheffert\.dotfiles\.editorconfig
+mklink C:\Users\tscheffert\.gitconfig C:\Users\tscheffert\.dotfiles\.gitconfig
+mklink C:\Users\tscheffert\.gitignore C:\Users\tscheffert\.dotfiles\.gitignore
+mklink C:\Users\tscheffert\.git-prompt.sh C:\Users\tscheffert\.dotfiles\.git-prompt.sh
+mklink C:\Users\tscheffert\.jscs.json C:\Users\tscheffert\.dotfiles\.jscs.json
+mklink C:\Users\tscheffert\.jshintrc C:\Users\tscheffert\.dotfiles\.jshintrc
+mklink C:\Users\tscheffert\.osx C:\Users\tscheffert\.dotfiles\.osx
+mklink C:\Users\tscheffert\.vimrc C:\Users\tscheffert\.dotfiles\.vimrc
+mklink C:\Users\tscheffert\coffeelint.json C:\Users\tscheffert\.dotfiles\coffeelint.json
+mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\tscheffert\.dotfiles\PowerShell\profile.ps1
+mklink C:\Users\tscheffert\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\tscheffert\.dotfiles\PowerShell\PowerTabConfig.xml
+mklink C:\Users\tscheffert\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\tscheffert\.dotfiles\PowerShell\TabExpansion.xml
+mklink C:\Users\tscheffert\.ptignore C:\Users\tscheffert\.dotfiles\.ptignore
 
 
 pause

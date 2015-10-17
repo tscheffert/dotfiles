dd0e48bc2e2302d1bcb385a706d0f3e58427fae5
Trent Scheffert <trent.scheffert@gmail.com>
2015-10-17 16:34:24 -0400

    Feat: Add rbenv plugins script

---
+++ bootstrap/rbenv-plugins.sh
@@ -0,0 +1,38 @@
+#! /usr/env/bin bash
+#
+# Install my preferred rbenv plugins!
+
+ensure_exists() {
+  if [[ ! -d $1 ]]; then
+    mkdir $1
+  fi
+}
+
+install_plugin() {
+  local github_path=$1
+  # Expand parameter to everything before '/'
+  local author=${github_path%/*}
+  # Expand parameter to everything after '/'
+  local plugin=${github_path#*/}
+
+  if [[ ! -d $rbenv_plugins_path/$plugin ]]; then
+    echo "Installing $plugin"
+    git clone -q https://github.com/$github_path $rbenv_plugins_path/$plugin \
+      || echo "Something's fucky with $plugin"
+  fi
+}
+
+# Assign to $RBENV_ROOT if it exists, else $HOME/.rbenv
+rbenv_path=${RBENV_ROOT:-$HOME/.rbenv}
+
+rbenv_plugins_path=$rbenv_path/plugins
+
+ensure_exists $rbenv_path
+ensure_exists $rbenv_plugins_path
+
+install_plugin sstephenson/rbenv-gem-rehash
+install_plugin sstephenson/rbenv-default-gems
+install_plugin tpope/rbenv-communal-gems
+install_plugin nicknovitski/rbenv-gem-update
+install_plugin rkh/rbenv-whatis
+install_plugin toy/rbenv-update-rubies

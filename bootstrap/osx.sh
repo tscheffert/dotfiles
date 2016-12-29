70eeeb99aaaf3e47537a96613766f154c33594c3
Trent Scheffert <trent.scheffert@gmail.com>
2016-12-29 17:10:51 -0600

    Fix: Don't mention Slate, it's not used

--- bootstrap/osx.sh
+++ bootstrap/osx.sh
@@ -12,7 +12,6 @@ exec $SHELL -l
 
 # --- Next Steps ---
 # 1. Setup iTerm2 to use it's preferences
-# 2. Download Slate and set it to always startup
 # 3. Figure out how to install exuberant-ctags
 # 4. ripper-tags from https://github.com/lzap/gem-ripper-tags
 # 5. brew install zsh zsh-completions

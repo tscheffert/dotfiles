14677c06658b0fc4fde271acfd61f3aabdb6ce07
Trent Scheffert <trent.scheffert@gmail.com>
2017-10-18 17:08:53 -0500

    Bootstrap: Fix bootstrap.bat missing double quote

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,28 +1,28 @@
-mklink /j C:\Users\Trent\bin C:\Users\Trent\.dotfiles\bin
-mklink /j C:\Users\Trent\vimfiles C:\Users\Trent\.dotfiles\vimfiles
-mklink /j C:\Users\Trent\.git_template C:\Users\Trent\.dotfiles\.git_template
-mklink /j C:\Users\Trent\.prompts C:\Users\Trent\.dotfiles\prompts
-mklink /j C:\Users\Trent\.zsh C:\Users\Trent\.dotfiles\zsh
-mklink C:\Users\Trent\.bash_profile C:\Users\Trent\.dotfiles\.bash_profile
-mklink C:\Users\Trent\.bashrc C:\Users\Trent\.dotfiles\.bashrc
-mklink "C:\\Program Files\\ConEmu\\ConEmu.xml C:\Users\Trent\.dotfiles\ConEmu.xml
-mklink C:\Users\Trent\.curlrc C:\Users\Trent\.dotfiles\.curlrc
-mklink C:\Users\Trent\.editorconfig C:\Users\Trent\.dotfiles\.editorconfig
-mklink C:\Users\Trent\.gitconfig C:\Users\Trent\.dotfiles\.gitconfig
-mklink C:\Users\Trent\.gitignore C:\Users\Trent\.dotfiles\.gitignore
-mklink C:\Users\Trent\.jscs.json C:\Users\Trent\.dotfiles\.jscs.json
-mklink C:\Users\Trent\.jshintrc C:\Users\Trent\.dotfiles\.jshintrc
-mklink C:\Users\Trent\.vimrc C:\Users\Trent\.dotfiles\.vimrc
-::mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\Trent\.dotfiles\PowerShell\profile.ps1
-::mklink C:\Users\Trent\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\Trent\.dotfiles\PowerShell\PowerTabConfig.xml
-::mklink C:\Users\Trent\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\Trent\.dotfiles\PowerShell\TabExpansion.xml
-mklink C:\Users\Trent\.agignore C:\Users\Trent\.dotfiles\.agignore
-mklink C:\Users\Trent\.pryrc C:\Users\Trent\.dotfiles\.pryrc
-mklink C:\Users\Trent\.gemrc C:\Users\Trent\.dotfiles\.gemrc
-mklink C:\Users\Trent\.tmux.conf C:\Users\Trent\.dotfiles\.tmux.conf
-mklink C:\Users\Trent\.zshrc C:\Users\Trent\.dotfiles\.zshrc
-mklink C:\Users\Trent\.aprc C:\Users\Trent\.dotfiles\.aprc
-mklink C:\Users\Trent\.ctags C:\Users\Trent\.dotfiles\.ctags
+mklink /j C:\Users\tscheffert\bin C:\Users\tscheffert\.dotfiles\bin
+mklink /j C:\Users\tscheffert\vimfiles C:\Users\tscheffert\.dotfiles\vimfiles
+mklink /j C:\Users\tscheffert\.git_template C:\Users\tscheffert\.dotfiles\.git_template
+mklink /j C:\Users\tscheffert\.prompts C:\Users\tscheffert\.dotfiles\prompts
+mklink /j C:\Users\tscheffert\.zsh C:\Users\tscheffert\.dotfiles\zsh
+mklink C:\Users\tscheffert\.bash_profile C:\Users\tscheffert\.dotfiles\.bash_profile
+mklink C:\Users\tscheffert\.bashrc C:\Users\tscheffert\.dotfiles\.bashrc
+mklink "C:\\Program Files\\ConEmu\\ConEmu.xml" C:\Users\tscheffert\.dotfiles\ConEmu.xml
+mklink C:\Users\tscheffert\.curlrc C:\Users\tscheffert\.dotfiles\.curlrc
+mklink C:\Users\tscheffert\.editorconfig C:\Users\tscheffert\.dotfiles\.editorconfig
+mklink C:\Users\tscheffert\.gitconfig C:\Users\tscheffert\.dotfiles\.gitconfig
+mklink C:\Users\tscheffert\.gitignore C:\Users\tscheffert\.dotfiles\.gitignore
+mklink C:\Users\tscheffert\.jscs.json C:\Users\tscheffert\.dotfiles\.jscs.json
+mklink C:\Users\tscheffert\.jshintrc C:\Users\tscheffert\.dotfiles\.jshintrc
+mklink C:\Users\tscheffert\.vimrc C:\Users\tscheffert\.dotfiles\.vimrc
+::mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\tscheffert\.dotfiles\PowerShell\profile.ps1
+::mklink C:\Users\tscheffert\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\tscheffert\.dotfiles\PowerShell\PowerTabConfig.xml
+::mklink C:\Users\tscheffert\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\tscheffert\.dotfiles\PowerShell\TabExpansion.xml
+mklink C:\Users\tscheffert\.agignore C:\Users\tscheffert\.dotfiles\.agignore
+mklink C:\Users\tscheffert\.pryrc C:\Users\tscheffert\.dotfiles\.pryrc
+mklink C:\Users\tscheffert\.gemrc C:\Users\tscheffert\.dotfiles\.gemrc
+mklink C:\Users\tscheffert\.tmux.conf C:\Users\tscheffert\.dotfiles\.tmux.conf
+mklink C:\Users\tscheffert\.zshrc C:\Users\tscheffert\.dotfiles\.zshrc
+mklink C:\Users\tscheffert\.aprc C:\Users\tscheffert\.dotfiles\.aprc
+mklink C:\Users\tscheffert\.ctags C:\Users\tscheffert\.dotfiles\.ctags
 
 
 pause

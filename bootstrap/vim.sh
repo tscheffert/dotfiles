d7319d552a00878352244e1a318dfd7c85aacb24
Trent Scheffert <trent.scheffert@gmail.com>
2017-10-27 10:29:14 -0500

    Bootstrap: Change vim boostrap on Windows vs other

--- bootstrap/vim.sh
+++ bootstrap/vim.sh
@@ -3,14 +3,30 @@
 echo ""
 echo " -- Setting up vim config --"
 
-# Setup vimfiles
-INSTALL_PATH="$HOME/.vim/autoload/plug.vim"
+# --- Platform ---
+# copied from .bash_profile
+# use: if [[ $platform == 'windows']]; then
+platform='unknown'
+if [[ "$OSTYPE" == "msys" ]]; then
+  platform='windows'
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  platform='windows'
+elif [[ "$OSTYPE" == "win32" ]]; then
+  platform='windows'
+fi
+
+# Set $INSTALL_PATH and download vim-plug if it doesn't exist there
+if [[ $platform == 'windows' ]]; then
+  INSTALL_PATH="$HOME/vimfiles/autoload/plug.vim"
+else
+  INSTALL_PATH="$HOME/.vim/autoload/plug.vim"
+fi
 if [[ ! -f $INSTALL_PATH ]]; then
   echo "Downloading vim-plug to $INSTALL_PATH"
   curl -fLo "$INSTALL_PATH" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
-  echo "Found existing $INSTALL_PATH directory"
+  echo "Found existing $INSTALL_PATH"
 fi
 
 echo "Running vim-plug Install"

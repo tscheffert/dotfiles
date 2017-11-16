75c63be241c40ba9e3fc36a9afb4d1862f79516f
Trent Scheffert <trent.scheffert@gmail.com>
2017-11-16 13:55:44 -0600

    Windows Bootstrapping: Use %userprofile% for paths

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,35 +1,32 @@
-mklink /j C:\Users\tscheffert\bin C:\Users\tscheffert\.dotfiles\bin
-mklink /j C:\Users\tscheffert\vimfiles C:\Users\tscheffert\.dotfiles\vimfiles
-mklink /j C:\Users\tscheffert\.git_template C:\Users\tscheffert\.dotfiles\.git_template
-mklink /j C:\Users\tscheffert\.prompts C:\Users\tscheffert\.dotfiles\prompts
-mklink /j C:\Users\tscheffert\.zsh C:\Users\tscheffert\.dotfiles\zsh
+mklink /j %userprofile%\bin %userprofile%\.dotfiles\bin
+mklink /j %userprofile%\vimfiles %userprofile%\.dotfiles\vimfiles
+mklink /j %userprofile%\.git_template %userprofile%\.dotfiles\.git_template
+mklink /j %userprofile%\.prompts %userprofile%\.dotfiles\prompts
+mklink /j %userprofile%\.zsh %userprofile%\.dotfiles\zsh
 mklink /j %userprofile%\AutoHotkey %userprofile%\.dotfiles\AutoHotkey
-mklink C:\Users\tscheffert\.bash_profile C:\Users\tscheffert\.dotfiles\.bash_profile
-mklink C:\Users\tscheffert\.bashrc C:\Users\tscheffert\.dotfiles\.bashrc
-mklink C:\Users\tscheffert\.inputrc C:\Users\tscheffert\.dotfiles\.inputrc
+mklink %userprofile%\.bash_profile %userprofile%\.dotfiles\.bash_profile
+mklink %userprofile%\.bashrc %userprofile%\.dotfiles\.bashrc
+mklink %userprofile%\.inputrc %userprofile%\.dotfiles\.inputrc
 mklink %userprofile%\.dir_colors mklink %userprofile%\.dotfiles\.dir_colors
-mklink "C:\\Program Files\\ConEmu\\ConEmu.xml" C:\Users\tscheffert\.dotfiles\ConEmu.xml
-mklink C:\Users\tscheffert\.curlrc C:\Users\tscheffert\.dotfiles\.curlrc
-mklink C:\Users\tscheffert\.editorconfig C:\Users\tscheffert\.dotfiles\.editorconfig
-mklink C:\Users\tscheffert\.gitconfig C:\Users\tscheffert\.dotfiles\.gitconfig
-mklink C:\Users\tscheffert\.gitignore C:\Users\tscheffert\.dotfiles\.gitignore
-mklink C:\Users\tscheffert\.jscs.json C:\Users\tscheffert\.dotfiles\.jscs.json
-mklink C:\Users\tscheffert\.jshintrc C:\Users\tscheffert\.dotfiles\.jshintrc
-mklink C:\Users\tscheffert\.vimrc C:\Users\tscheffert\.dotfiles\.vimrc
+mklink "C:\\Program Files\\ConEmu\\ConEmu.xml" %userprofile%\.dotfiles\ConEmu.xml
+mklink %userprofile%\.curlrc %userprofile%\.dotfiles\.curlrc
+mklink %userprofile%\.editorconfig %userprofile%\.dotfiles\.editorconfig
+mklink %userprofile%\.gitconfig %userprofile%\.dotfiles\.gitconfig
+mklink %userprofile%\.gitignore %userprofile%\.dotfiles\.gitignore
+mklink %userprofile%\.jscs.json %userprofile%\.dotfiles\.jscs.json
+mklink %userprofile%\.jshintrc %userprofile%\.dotfiles\.jshintrc
+mklink %userprofile%\.vimrc %userprofile%\.dotfiles\.vimrc
 mkdir %userprofile%\Documents\WindowsPowerShell
-mklink C:\Users\tscheffert\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 C:\Users\tscheffert\.dotfiles\powershell\Microsoft.PowerShell_profile.ps1
-::mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 C:\Users\tscheffert\.dotfiles\PowerShell\profile.ps1
-::mklink C:\Users\tscheffert\Documents\WindowsPowerShell\PowerTabConfig.xml C:\Users\tscheffert\.dotfiles\PowerShell\PowerTabConfig.xml
-::mklink C:\Users\tscheffert\Documents\WindowsPowerShell\TabExpansion.xml C:\Users\tscheffert\.dotfiles\PowerShell\TabExpansion.xml
-mklink C:\Users\tscheffert\.agignore C:\Users\tscheffert\.dotfiles\.agignore
-mklink C:\Users\tscheffert\.pryrc C:\Users\tscheffert\.dotfiles\.pryrc
+mklink %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %userprofile%\.dotfiles\powershell\Microsoft.PowerShell_profile.ps1
+mklink %userprofile%\.agignore %userprofile%\.dotfiles\.agignore
+mklink %userprofile%\.pryrc %userprofile%\.dotfiles\.pryrc
 mklink %userprofile%\.irbrc %userprofile%\.dotfiles\.irbrc
 mklink %userprofile%\console_config.rb %userprofile%\.dotfiles\console_config.rb
-mklink C:\Users\tscheffert\.gemrc C:\Users\tscheffert\.dotfiles\.gemrc
-mklink C:\Users\tscheffert\.tmux.conf C:\Users\tscheffert\.dotfiles\.tmux.conf
-mklink C:\Users\tscheffert\.zshrc C:\Users\tscheffert\.dotfiles\.zshrc
-mklink C:\Users\tscheffert\.aprc C:\Users\tscheffert\.dotfiles\.aprc
-mklink C:\Users\tscheffert\.ctags C:\Users\tscheffert\.dotfiles\.ctags
+mklink %userprofile%\.gemrc %userprofile%\.dotfiles\.gemrc
+mklink %userprofile%\.tmux.conf %userprofile%\.dotfiles\.tmux.conf
+mklink %userprofile%\.zshrc %userprofile%\.dotfiles\.zshrc
+mklink %userprofile%\.aprc %userprofile%\.dotfiles\.aprc
+mklink %userprofile%\.ctags %userprofile%\.dotfiles\.ctags
 mklink %userprofile%\.yamllint mklink %userprofile%\.dotfiles\.yamllint
 
 pause

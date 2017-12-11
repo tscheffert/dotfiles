3a229aa9e9b7eb9a608d89b2bb9c75c568c7ad04
Trent Scheffert <trent.scheffert@gmail.com>
2017-12-11 10:44:53 -0600

    Windows: Organize bootstrap.bat

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -4,29 +4,31 @@ mklink /j %userprofile%\.git_template %userprofile%\.dotfiles\.git_template
 mklink /j %userprofile%\.prompts %userprofile%\.dotfiles\prompts
 mklink /j %userprofile%\.zsh %userprofile%\.dotfiles\zsh
 mklink /j %userprofile%\AutoHotkey %userprofile%\.dotfiles\AutoHotkey
+mklink %userprofile%\.agignore %userprofile%\.dotfiles\.agignore
+mklink %userprofile%\.aprc %userprofile%\.dotfiles\.aprc
 mklink %userprofile%\.bash_profile %userprofile%\.dotfiles\.bash_profile
 mklink %userprofile%\.bashrc %userprofile%\.dotfiles\.bashrc
-mklink %userprofile%\.inputrc %userprofile%\.dotfiles\.inputrc
-mklink %userprofile%\.dir_colors mklink %userprofile%\.dotfiles\.dir_colors
-mklink "C:\\Program Files\\ConEmu\\ConEmu.xml" %userprofile%\.dotfiles\ConEmu.xml
+mklink %userprofile%\.ctags %userprofile%\.dotfiles\.ctags
 mklink %userprofile%\.curlrc %userprofile%\.dotfiles\.curlrc
+mklink %userprofile%\.dir_colors mklink %userprofile%\.dotfiles\.dir_colors
 mklink %userprofile%\.editorconfig %userprofile%\.dotfiles\.editorconfig
+mklink %userprofile%\.gemrc %userprofile%\.dotfiles\.gemrc
 mklink %userprofile%\.gitconfig %userprofile%\.dotfiles\.gitconfig
 mklink %userprofile%\.gitignore %userprofile%\.dotfiles\.gitignore
+mklink %userprofile%\.inputrc %userprofile%\.dotfiles\.inputrc
+mklink %userprofile%\.irbrc %userprofile%\.dotfiles\.irbrc
 mklink %userprofile%\.jscs.json %userprofile%\.dotfiles\.jscs.json
 mklink %userprofile%\.jshintrc %userprofile%\.dotfiles\.jshintrc
+mklink %userprofile%\.pryrc %userprofile%\.dotfiles\.pryrc
+mklink %userprofile%\.tmux.conf %userprofile%\.dotfiles\.tmux.conf
 mklink %userprofile%\.vimrc %userprofile%\.dotfiles\.vimrc
+mklink %userprofile%\.yamllint mklink %userprofile%\.dotfiles\.yamllint
+mklink %userprofile%\.zshrc %userprofile%\.dotfiles\.zshrc
+mklink %userprofile%\console_config.rb %userprofile%\.dotfiles\console_config.rb
+
 mkdir %userprofile%\Documents\WindowsPowerShell
 mklink %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %userprofile%\.dotfiles\powershell\Microsoft.PowerShell_profile.ps1
-mklink %userprofile%\.agignore %userprofile%\.dotfiles\.agignore
-mklink %userprofile%\.pryrc %userprofile%\.dotfiles\.pryrc
-mklink %userprofile%\.irbrc %userprofile%\.dotfiles\.irbrc
-mklink %userprofile%\console_config.rb %userprofile%\.dotfiles\console_config.rb
-mklink %userprofile%\.gemrc %userprofile%\.dotfiles\.gemrc
-mklink %userprofile%\.tmux.conf %userprofile%\.dotfiles\.tmux.conf
-mklink %userprofile%\.zshrc %userprofile%\.dotfiles\.zshrc
-mklink %userprofile%\.aprc %userprofile%\.dotfiles\.aprc
-mklink %userprofile%\.ctags %userprofile%\.dotfiles\.ctags
-mklink %userprofile%\.yamllint mklink %userprofile%\.dotfiles\.yamllint
+
+mklink "C:\\Program Files\\ConEmu\\ConEmu.xml" %userprofile%\.dotfiles\ConEmu.xml
 
 pause

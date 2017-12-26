d6b54a00451e336b49bd9238db2741bd54ed438b
Trent Scheffert <trent.scheffert@gmail.com>
2017-12-25 18:47:16 -0600

    Scripts: Updates to detect_black.sh

--- windows-scripts/split-videos/detect_black.sh
+++ windows-scripts/split-videos/detect_black.sh
@@ -1,3 +1,3 @@
 ffmpeg="/c/tools/ffmpeg-3.2.4-win64-static/bin/ffmpeg.exe"
-video="???"
-"$ffmpeg" -i "$video" -vf blackdetect=d=2:pic_th=0.98:pix_th=0.15 -an -f null -
+video="$1"
+"$ffmpeg" -i "$video" -vf blackdetect=d=1:pic_th=0.85:pix_th=0.15 -an -f null -

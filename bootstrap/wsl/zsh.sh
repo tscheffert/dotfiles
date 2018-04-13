91107e02d84e90f6cf0afd1494ee0d52504ee739
Trent Scheffert <trent.scheffert@gmail.com>
2018-04-13 15:41:54 -0500

    WSL: Redirect STDERR to STDOUT when using apt

--- bootstrap/wsl/zsh.sh
+++ bootstrap/wsl/zsh.sh
@@ -19,10 +19,10 @@ ensure_latest_zsh() {
 
   echo ""
   echo " -- Ensuring Zsh is installed and up to date --"
-  if [[ ! "$(apt list zsh --installed)" =~ '[installed]' ]]; then
+  if [[ ! "$(apt list zsh --installed 2>&1)" =~ '[installed]' ]]; then
     echo "You don't have Zsh installed, installing..."
     sudo apt-get install zsh --assume-yes
-  elif [[ "$(apt list --upgradeable zsh)" =~ 'zsh' ]]; then
+  elif [[ "$(apt list --upgradeable zsh 2>&1)" =~ 'zsh' ]]; then
     echo "Your installed Zsh is outdated, upgrading..."
     sudo apt-get upgrade zsh --assume-yes
   else
@@ -45,10 +45,10 @@ ensure_latest_zsh() {
 
   echo ""
   echo " -- Ensuring you have an updated zsh-syntax-highlighting installed --"
-  if [[ ! "$(apt list --installed zsh-syntax-highlighting)" =~ '[installed]' ]]; then
+  if [[ ! "$(apt list --installed zsh-syntax-highlighting 2>&1)" =~ '[installed]' ]]; then
     echo "You don't have zsh-syntax-highlighting installed, installing..."
     sudo apt-get install zsh-syntax-highlighting --assume-yes
-  elif [[ "$(apt list --upgradeable zsh-syntax-highlighting)" =~ 'zsh-syntax-highlighting' ]]; then
+  elif [[ "$(apt list --upgradeable zsh-syntax-highlighting 2>&1)" =~ 'zsh-syntax-highlighting' ]]; then
     echo "Your installed zsh-syntax-highlighting is outdated, upgrading..."
     sudo apt-get upgrade zsh-syntax-highlighting --assume-yes
   else
@@ -56,11 +56,11 @@ ensure_latest_zsh() {
   fi
 
   echo " -- Ensuring you have an updated zsh-completions --"
-  if [[ ! "$(apt list --installed zsh-completions)" =~ '[installed]' ]]; then
+  if [[ ! "$(apt list --installed zsh-completions 2>&1)" =~ '[installed]' ]]; then
     echo "You don't have zsh-completions installed, installing..."
     add_opensuse_zshcompletions_to_apt
     sudo apt-get install zsh-completions --assume-yes
-  elif [[ "$(apt list --upgradeable zsh-completions)" =~ 'zsh-completions' ]]; then
+  elif [[ "$(apt list --upgradeable zsh-completions 2>&1)" =~ 'zsh-completions' ]]; then
     echo "Your installed zsh-completions is outdated, upgrading..."
     sudo apt-get upgrade zsh-completions --assume-yes
   else

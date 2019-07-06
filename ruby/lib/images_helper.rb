08e0df2a4fde56cf584a30c4994dce4564500121
Trent Scheffert <trent.scheffert@gmail.com>
2019-07-06 14:12:03 -0500

    Ruby: More image scripts and improvements

--- ruby/lib/images_helper.rb
+++ ruby/lib/images_helper.rb
@@ -14,6 +14,7 @@ module ImagesHelper
     TOO_SMALL_DIR = '_too_small'.freeze
     LANDSCAPE_DIR = '_landscape'.freeze
     PORTRAIT_DIR = '_portrait'.freeze
+    FLAT_DIRS_DIR = '_flat_dirs'.freeze
 
     IGNORED_DIRS = [
       BAD_DIR,
@@ -25,10 +26,36 @@ module ImagesHelper
       '..'
     ].freeze
 
-    MIN_WIDTH = 2560
+    MIN_WIDTH = 2495
     MIN_HEIGHT = 1440
     MIN_RATIO = BigDecimal('1.77')
     MAX_RATIO = BigDecimal('1.78')
 
   end
+
+  def self.all_images_in_dir(dir)
+    Dir.entries(dir).select do |f|
+      File.file?(f) && Constants::IMAGE_EXTENSIONS.include?(File.extname(f))
+    end
+  end
+
+  def self.safe_rename(old, new)
+    FileUtils.mv(old, new)
+  rescue => e
+    puts "File #{old} -> #{new} failed with error #{e}"
+  end
+
+  # Escape the meta chars that Dir::glob uses
+  def self.safe_path_for_glob(path)
+    # Backslash only for escaping, switch it to forward slashes
+    path = path.gsub(/\\/, '/')
+
+    path = path.gsub(/\*/, '\*')
+    path = path.gsub(/\?/, '\?')
+    path = path.gsub(/\[/, '\[')
+    path = path.gsub(/\]/, '\]')
+    path = path.gsub(/\{/, '\{')
+    path = path.gsub(/\}/, '\}')
+  end
+
 end

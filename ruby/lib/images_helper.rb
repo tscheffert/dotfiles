tscheffert/dotfiles

commit ab90f23cdf73641b90a6109b1368f21678dad04d

Bin: Add guards in sort_folders_by_prefix for length

bin/sort_folders_by_prefix
@@ -54,6 +54,17 @@ module SortFoldersByPrefix
       end
     end
 
+    to_sort_dirs = to_sort_dirs.reject do |dir|
+      if dir.include?('Seiheki Chousadan')
+        puts "Dir: #{dir}"
+        puts "Length: #{File.absolute_path(dir).length}"
+      end
+      too_long = File.absolute_path(dir).length >= 255
+      warn "Directory Name too long for #{dir}" if too_long
+
+      too_long
+    end
+
     puts "Already sorted directories:\n  #{already_sorted_dirs.join("\n  ")}"
 
     already_sorted_dirs.each do |dir|
@@ -94,8 +105,12 @@ module SortFoldersByPrefix
       FileUtils.mkdir(sorted_dir_name) if !@test_run
     end
 
-    puts "Moving dir '#{basename_dir}' into '#{sorted_dir_name}'" if @debug
-    FileUtils.mv(dir, sorted_dir_name) if !@test_run
+    if absolute_dir.length + sorted_dir_name.length > 255
+      warn "Cannot rename '#{basename_dir}', sorted name is too long"
+    else
+      puts "Moving dir '#{basename_dir}' into '#{sorted_dir_name}'" if @debug
+      FileUtils.mv(dir, sorted_dir_name) if !@test_run
+    end
   end
   private_class_method :sort_dir
 

commit aec7d656bcbe142046174be54154e6aebf168abe

Bin: Warn on length issues in ImagesHelper
Also fixes bug introduced by ab90f23cdf73641b90a6109b1368f21678dad04d
where the length guard wasn't actually working as expected.

bin/sort_folders_by_prefix
@@ -54,17 +54,6 @@ module SortFoldersByPrefix
       end
     end
 
-    to_sort_dirs = to_sort_dirs.reject do |dir|
-      if dir.include?('Seiheki Chousadan')
-        puts "Dir: #{dir}"
-        puts "Length: #{File.absolute_path(dir).length}"
-      end
-      too_long = File.absolute_path(dir).length >= 255
-      warn "Directory Name too long for #{dir}" if too_long
-
-      too_long
-    end
-
     puts "Already sorted directories:\n  #{already_sorted_dirs.join("\n  ")}"
 
     already_sorted_dirs.each do |dir|

ruby/lib/images_helper.rb
@@ -136,7 +136,11 @@ module ImagesHelper
   end
 
   def self.all_dirs_in(dir:)
-    Dir.entries(dir, encoding: 'UTF-8').select { |entry| File.directory?(entry) } - Constants::IGNORED_DIRS
+    Dir.entries(dir, encoding: 'UTF-8').select do |entry|
+      warn "WARNING: Length of directory #{entry} is too long, cannot process it" if File.absolute_path(entry).length >= 255
+
+      File.directory?(entry)
+    end - Constants::IGNORED_DIRS
   end
 
   def self.safe_rename(old, new)